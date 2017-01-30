67a695a7307ee783217a5944db3ec242233e169e
Varun Dey <varun_dey@m2711322hg8wp.symc.symantec.com>
2017-01-30 17:00:38 +0530

    uses promises :raised_hand:

--- background.js
+++ background.js
@@ -1,46 +1,45 @@
 var key = "your API key here";
 
-function getCurrentTabUrl(callback) {
-  var queryInfo = {
+function getCurrentTabUrl(callback){
+  var queryInfo={
     active: true,
     currentWindow: true
   };
-  chrome.tabs.query(queryInfo, function(tabs) {
-    var tab = tabs[0];
-    var url = tab.url;
-    console.assert(typeof url == 'string', 'tab.url should be a string');
-    callback(url);
+  chrome.tabs.query(queryInfo, function(tabs){
+    callback(tabs[0].url);
   });
 }
 
-function renderStatus(statusText) {
-  document.getElementById('result').textContent = statusText;
+function getShortURL(url){
+  return new Promise(function(resolve, reject) {
+    var API_URL = 'https://www.googleapis.com/urlshortener/v1/url?fields=id&key=' + key;
+    var xml = new XMLHttpRequest();
+    xml.open('POST', API_URL, true);
+    xml.setRequestHeader("Content-Type", "application/json");
+    xml.send(JSON.stringify({"longUrl": url}));
+    xml.onload = function () {
+      var response = JSON.parse(xml.response);
+      if (response.id === undefined){
+        reject(Error("Check active tab URL"));
+      }
+      else{
+        resolve(response);
+      }
+    }
+  });
 }
 
-function getShortURL(LongURL, callback) {
-  var API_URL = 'https://www.googleapis.com/urlshortener/v1/url?fields=id&key=' + key;
-
-  var xml = new XMLHttpRequest();
-  xml.open('POST', API_URL, true);
-  xml.setRequestHeader("Content-Type", "application/json");
-  //console.log(Url, searchTerm);
-  xml.send(JSON.stringify({"longUrl": LongURL}));
-  console.log(xml);
-  xml.onload = function(){
-    var response = xml.response;
-    callback(response);
-  };
+function renderStatus(text){
+  document.getElementById('result').textContent = text;
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  getCurrentTabUrl(function(url) {
-    getShortURL(url, function(result) {
-      result = JSON.parse(result);
-      console.log(result.id);
-      if (result.id === undefined){
-        return renderStatus("Check your tab URL");
-      };
-      renderStatus(result.id);
-      });
+document.addEventListener('DOMContentLoaded', function(){
+  getCurrentTabUrl(function(url){
+    getShortURL(url).then(function (fulfilled){
+      console.log(fulfilled.id);
+      return renderStatus(fulfilled.id);
+    }, function(error){
+      renderStatus("Check active tab URL");
+    });
   });
 });
